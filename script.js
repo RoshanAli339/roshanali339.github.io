336805bfbb042e454914404724eadbbc245dfaea
RoshanAli339 <roshanali339lps@gmail.com>
2023-01-01 18:26:39 +0530

    Sunday 01 January 2023 06:26:39 PM IST

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const text = document.querySelector(".sec-text");
-
+const hi = document.querySelector("#hi");
 const textLoad = () => {
   setTimeout(() => {
+    hi.textContent = "Hi!";
     text.textContent = "Roshan Ali";
   }, 0);
   setTimeout(() => {
+    hi.textContent = "";
     text.textContent = "a Student";
   }, 3000);
   setTimeout(() => {
+    hi.textContent = "";
     text.textContent = "a Programmer";
   }, 6000);
   setTimeout(() => {
+    hi.textContent = "";
     text.textContent = "a Web Developer";
   }, 9000); 
 }
 
 textLoad();
 setInterval(textLoad, 12000);
+console.log(hi.innerHTML);
